89e17ee2ce86182c272bb60cbb5e0efd4feec8b6
kalosa0770 <kalosaelijah3@gmail.com>
2025-10-15 15:15:50 +0200

    node_modules

--- golden-swift-bank/vite.config.js
+++ golden-swift-bank/vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  
-  // Use "/" as base so Vercel serves from the root
   base: '/',
-  
-  // Add this proxy for local development
   server: {
     proxy: {
       '/api': 'http://localhost:3001'
